b26e1d5ad8bacff5172a76bf7a0d8ac9ee5e5e27
Nadzeya Rakach <nadezda.rakach@gmail.com>
2023-04-15 18:58:09 +0200

    The task is completed.

--- test_homework.py
+++ test_homework.py
@@ -1,15 +1,18 @@
-
+import math
+import random
 
 def test_greeting():
     """
     Напишите программу, которая выводит на экран приветствие.
     """
-    name = "Анна"
+    name = 'Анна'
     age = 25
     # TODO Сформируйте нужную строку
-    output = ""
+    output = f"Привет, {name}! Тебе {age} лет."
+    print(output)
     # Проверяем результат
-    assert output == "Привет, Анна! Тебе 25 лет."
+    assert output == 'Привет, Анна! Тебе 25 лет.'
+
 
 
 def test_rectangle():
@@ -21,11 +24,11 @@ def test_rectangle():
     b = 20
 
     # TODO сосчитайте периметр
-    perimeter = 0
+    perimeter = 2 * (a + b)
     assert perimeter == 60
 
     # TODO сосчитайте площадь
-    area = 0
+    area = a * b
     assert area == 200
 
 
@@ -36,11 +39,11 @@ def test_circle():
     """
     r = 23
     # TODO сосчитайте площадь
-    area = 0
+    area = math.pi * r**2
     assert area == 1661.9025137490005
 
     # TODO сосчитайте длину окружности
-    length = 0
+    length = 2 * math.pi * r
     assert length == 144.51326206513048
 
 
@@ -51,6 +54,9 @@ def test_random_list():
 
     # TODO создайте список
     l = []
+    for i in range(10):
+        l.append(random.randint(1, 100))
+        l.sort()
     assert len(l) == 10
     assert l[0] < l[-1]
 
@@ -61,7 +67,7 @@ def test_unique_elements():
     """
     l = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 9, 10, 10]
     # TODO удалите повторяющиеся элементы
-
+    l = list(set(l))
     assert isinstance(l, list)
     assert len(l) == 10
     assert l == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -77,7 +83,7 @@ def test_dicts():
     first = ["a", "b", "c", "d", "e"]
     second = [1, 2, 3, 4, 5]
     # TODO создайте словарь
-    d = {}
+    d = dict(zip(first, second))
 
     assert isinstance(d, dict)
     assert len(d) == 5
